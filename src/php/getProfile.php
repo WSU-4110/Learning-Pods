4ee8658fa28a184012392d0749eab29fcd9ecc8e
gs3935 <gs3935@wayne.edu>
2020-10-15 16:17:50 -0500

    Changes to profile.html and php
    Made and altered the Profile and related PHP pages

---
+++ src/php/getProfile.php
@@ -0,0 +1,29 @@
+<?php
+
+    // Define your connection properties
+    $servername  = "localhost";
+    $dbname = "id15127113_learningpodsdb";
+    $username = "id15127113_admin";
+    $password = "Jefferson2020!!";
+
+    // Create connection
+    $con=mysqli_connect($servername,$username,$password,$dbname);
+    
+    if (mysqli_connect_errno())
+   {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
+   }
+
+
+    mysqli_query($con,"INSERT INTO People(LastName, FirstName, Birthday, ZipCode) VALUES ('Kirton','Dakota','1994-01-10',48312)");
+    
+    $result = mysqli_query($con,"SELECT * FROM People;"); 
+   while($row = mysqli_fetch_array($result))
+   {
+      echo $row['FirstName'] . " " . $row['LastName'];
+      echo "<br>";
+   }
+    
+   mysqli_close($con);
+?>
+
