b9d7f714db89eb04796a3c1419f95b6c466fefe3
Dakota Kirton <71674789+gs3935@users.noreply.github.com>
2020-10-15 16:10:08 -0500

    Update SimpleQueries.sql
    Added additional sample queries with examples for join statements and for subqueries

--- SimpleQueries.sql
+++ SimpleQueries.sql
@@ -6,3 +6,17 @@ select
 	ZipCode,
 	year(GetDate()) - year(birthday) as [Age]
 from People;
+
+
+select FirstName, LastName, Street
+from People join HostHome
+	on People.PeopleID = HostHome.ParentID;
+
+
+select FirstName, LastName, Grade
+from Child join People
+on Child.ChildID = People.PeopleID
+where Child.ParentID = (Select ParentID from Parent
+						join People 
+						on People.PeopleID = Parent.ParentID
+						where LastName = 'Wagner');
