e9042c9432d87de146dd67f0dd882adba0e0d38b
gs3935 <gs3935@wayne.edu>
2020-10-28 18:57:21 -0500

    Added config file
    This file helps to facilitate connecting to the database for all php related files that need access

---
+++ src/php/config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'id15127113_admin');
+   define('DB_PASSWORD', 'Jefferson2020!!');
+   define('DB_DATABASE', 'id15127113_learningpodsdb');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
