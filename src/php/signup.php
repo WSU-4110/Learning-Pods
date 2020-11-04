d0f946a8e9e27ed8681126c08baedcf937f43a34
JackLacassee <71735563+JackLacassee@users.noreply.github.com>
2020-11-04 18:30:07 -0500

    revamped signup.php
    interacts better with other pages and the SQL database

--- src/php/signup.php
+++ src/php/signup.php
@@ -1,3 +1,25 @@
+<?php
+	include("config.php");
+
+
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		if(! $db ) {
+		   die('Could not connect: ' . mysql_error());
+		}
+		
+		
+	   $username = $_POST['username'];
+	   $password = $_POST['password'];
+		
+		mysqli_query($db, "INSERT INTO LogOn ". "(UserName,UserPassword) ". 
+		"VALUES('$username','$password')");
+		
+		header("location: login.php");
+		 
+		mysqli_close($db);
+   }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,56 +29,27 @@
 </head>
 
 <body>
+    <div align = "center">
     <div class="container">
         <div class="row">
             <div class="col-md-4 offset-md-4 form">
-                <form action="signup.php" method="POST" autocomplete="">
-                    <h2 class="text-center">Learning Pods Signup</h2>
-                    <p class="text-center">We just need a few things:</p>
-                    <?php
-                    if(count($errors) == 1){
-                        ?>
-                        <div class="alert alert-danger text-center">
-                            <?php
-                            foreach($errors as $showerror){
-                                echo $showerror;
-                            }
-                            ?>
-                        </div>
-                        <?php
-                    }elseif(count($errors) > 1){
-                        ?>
-                        <div class="alert alert-danger">
-                            <?php
-                            foreach($errors as $showerror){
-                                ?>
-                                <li><?php echo $showerror; ?></li>
-                                <?php
-                            }
-                            ?>
-                        </div>
-                        <?php
-                    }
-                    ?>
-                    <div class="form"> <!-- calling this in every 2 lines was not necessary -->
-                        <input class="form-control" type="text" name="name" placeholder="Full Name" required value="<?php echo $name ?>">
-                        <br>
-                        <input class="form-control" type="email" name="email" placeholder="Email Address" required value="<?php echo $email ?>">
-                        <br>
-                        <input class="form-control" type="password" name="password" placeholder="Password" required>
-                        <br>
-                        <input class="form-control" type="password" name="cpassword" placeholder="Confirm password" required>
-                        
-                        <br><!-- PRIVACY POLICY -->
-                        <input class="form-control" type="radio" name="Terms" required value="1"><label for="Terms"> I aggree to <a href="../policy.html">Privacy Policy</a></label><br><br>
-                    
-                        <br>
-                        <input class="form-control button" type="submit" name="signup" value="Signup">
-
-                        <span class="forgotpass">Already a member? <a href="login.html" alt="pass"">Login</a></span>
-                    </div>
+				<h2>Learning Pods Signup</h2>
+				<p>Please enter the following information to create your Learning Pods account!</p>
+				
+				<form action="<?php $_PHP_SELF ?>" method="post">
+					<label for="username">Username:</label><br>
+					<input type="text" id="username" name="username" value="username"><br><br>
+					<label for="password">Password:</label><br>
+					<input type="text" id="password" name="password" value="password"><br><br>
+					<input type="radio" name="Terms" required value="1"><label for="Terms"> I agree to the <a href="policy.html">Privacy policy</a></label><br><br>
+					<input type="submit" id="submit" class="button" value="Submit">
+				</form>
+					<span class="forgotpass">Already a member? <a href="login.php" alt="pass">Login</a></span>
+                  </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </body>
 </html>
