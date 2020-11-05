fec181a340f22cc6198d723c97fdd37346aad886
JackLacassee <71735563+JackLacassee@users.noreply.github.com>
2020-11-05 17:06:51 -0500

    added user input error handling
    made sure that users cannot send missing or partial sign-up/login data into the database

--- src/login.php
+++ src/login.php
@@ -36,8 +36,8 @@
             <div style = "margin:30px">
                
                <form action = "" method = "post">
-                  <label>Username </label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password </label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <label>Username </label><input type = "text" name = "username" class = "box" required><br /><br />
+                  <label>Password </label><input type = "password" name = "password" class = "box" required><br/><br />
                   <input type = "submit" value = " Submit "/><br />
                </form>
 			   <span>New User? <a href="signup.php" alt="signup"">Sign up!</a></span>
