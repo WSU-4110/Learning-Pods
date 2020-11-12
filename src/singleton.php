bc47303d1869f064cb988a2bcf76f1ead17f9877
Dakota Kirton <71674789+gs3935@users.noreply.github.com>
2020-11-12 17:55:15 -0600

    Added singleton version of config.php
    Using Singleton to connect to the database instead of the config.php

---
+++ src/singleton.php
@@ -0,0 +1,24 @@
+<?php
+	class DB_Connect {
+		public $DB_SERVER;
+		public $DB_USERNAME;
+		public $DB_PASSWORD;
+		public $DB_DATABASE;
+		public static $instance;
+		public $db;
+		
+		function __construct($server, $username, $password, $databse) {
+			$this->DB_SERVER = $server;
+			$this->DB_USERNAME = $username;
+			$this->DB_PASSWORD = $password;
+			$this->DB_DATABASE = $databse;
+			$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		}
+		static function testConnection($server, $username, $password, $databse) {
+			if (!$db) {
+				$instance = new DB_Connect($server, $username, $password, $databse);
+			}
+			return $instance;
+		}
+	}
+?>
