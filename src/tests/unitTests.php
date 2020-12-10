3400b1dc3eb63b029d68b597f28a61f6a73abac0
gs3935 <gs3935@wayne.edu>
2020-12-09 22:06:52 -0600

    Adding Unit tests
    Co-Authored-By: Rob Rososko <r-ghst@users.noreply.github.com>

--- src/tests/unitTests.php
+++ src/tests/unitTests.php
@@ -1,10 +1,15 @@
 <?
-namespace Tests\Feature;
-
-class UnitTests extends TestCase {
-		public function loginTest() {
-			
-		}
-	
+use PHPUnit\Framework\TestCase;  
+    
+class TestFiles extends TestCase  
+{  
+    public function testNegativeTestcaseForAssertFileExists()  
+    {  
+        $file = "../login.php";   
+        $this->assertFileExists(  
+            $filename,  
+            "given filename doesn't exists"
+        );  
+    }  
 }
 ?>
